83f81d399f95fc4194fb4fd2840f85b9bccfae83
Armando Salazar <armandosalazar351@gmail.com>
2022-08-30 22:42:39 -0500

    Styles added

--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -29,7 +29,13 @@ export default {
 </script>
 
 <template>
+  <h1>Tags</h1>
   <div class="inputTag">
+    <input type="text" v-model="currentValue" @keydown="handleKeyDown" />
+    <!-- Otra forma de hacerlo -->
+    <!-- <form @submit.prevent="handleSubmit">
+        <input type="text" v-model="currentValue">
+      </form> -->
     <div class="tags">
       <div class="tag" v-for="(tag, index) in tags" :key="index">
         {{ tag }}
@@ -38,26 +44,40 @@ export default {
         </span>
       </div>
     </div>
-    <input type="text" v-model="currentValue" @keydown="handleKeyDown" />
-    <!-- Otra forma de hacerlo -->
-    <!-- <form @submit.prevent="handleSubmit">
-        <input type="text" v-model="currentValue">
-      </form> -->
   </div>
 </template>
 
-<style>
-.tags {
-  display: flex;
-}
-.tag {
-  padding: 2px;
-  display: flex;
-  align-items: center;
-  /* border: 1px solid #ccc; */
-}
-.tag span {
-  cursor: pointer;
-  /* font-size: 25px; */
-}
+<style scoped>
+  h1 {
+    margin-top: 40px;
+  }
+  .inputTag {
+    width: 90%;
+    margin: 0 auto;
+  }
+
+  .inputTag input {
+    margin: 20px 0;
+  }
+
+  .tags {
+    max-width: 100%;
+    display: flex;
+    /* overflow: auto; */
+    flex-wrap: wrap;
+  }
+
+  .tag {
+    padding: 2px 4px 2px 10px;
+    display: flex;
+    align-items: center;
+    border: 1px solid #ccc;
+    margin: 5px;
+    border-radius: 3px;
+  }
+
+  .tag span {
+    cursor: pointer;
+    font-size: 20px;
+  }
 </style>
